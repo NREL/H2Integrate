b29982845a5290cedf8a537b3fba7bfed9cb0f98
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-02-04 15:33:40 -0700

    changed input for custom pem cost

--- greenheart/simulation/technologies/hydrogen/electrolysis/PEM_costs_custom.py
+++ greenheart/simulation/technologies/hydrogen/electrolysis/PEM_costs_custom.py
@@ -1,4 +1,4 @@
-def calc_custom_electrolysis_capex_fom(electrolyzer_physics_results, electrolyzer_config):
+def calc_custom_electrolysis_capex_fom(electrolyzer_capacity_kW, electrolyzer_config):
     """calculates electrolyzer total installed capex and fixed O&M based on user-input values.
     Only used if greenheart_config["electrolyzer"]["cost_model"] is set to "custom"
     Requires additional inputs in greenheart_config["electrolyzer"]:
@@ -6,17 +6,16 @@ def calc_custom_electrolysis_capex_fom(electrolyzer_physics_results, electrolyze
         - electrolyzer_capex: electrolyzer capex in $/kW
 
     Args:
-        electrolyzer_physics_results (dict): results from electrolysis.run_electrolyzer_physics
+        electrolyzer_capacity_kW (float or int): electrolyzer capacity in kW
         electrolyzer_config (dict): greenheart_config["electrolyzer"]
 
     Returns:
         float: electrolyzer overnight capex in $
         float: electrolyzer fixed O&M in $/year
     """
-    capacity_kW = electrolyzer_physics_results["H2_Results"]["system capacity [kW]"]
-    electrolyzer_capex = electrolyzer_config["electrolyzer_capex"] * capacity_kW
+    electrolyzer_capex = electrolyzer_config["electrolyzer_capex"] * electrolyzer_capacity_kW
     if "fixed_om" in electrolyzer_config.keys():
-        electrolyzer_fopex = electrolyzer_config["fixed_om"] * capacity_kW
+        electrolyzer_fopex = electrolyzer_config["fixed_om"] * electrolyzer_capacity_kW
     else:
         electrolyzer_fopex = 0.0
     return electrolyzer_capex, electrolyzer_fopex
