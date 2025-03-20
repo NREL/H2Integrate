05ee5c93ed829a707903ca8647625898b843e889
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-03-20 12:59:46 -0600

    removed commented out code and updated some docstrings

--- greenheart/simulation/technologies/hydrogen/electrolysis/PEM_costs_custom.py
+++ greenheart/simulation/technologies/hydrogen/electrolysis/PEM_costs_custom.py
@@ -1,5 +1,6 @@
 def calc_custom_electrolysis_capex_fom(electrolyzer_capacity_kW, electrolyzer_config):
-    """calculates electrolyzer total installed capex and fixed O&M based on user-input values.
+    """Calculates electrolyzer total installed capex and fixed O&M based on user-input values.
+
     Only used if greenheart_config["electrolyzer"]["cost_model"] is set to "custom"
     Requires additional inputs in greenheart_config["electrolyzer"]:
         - fixed_om_per_kw: electrolyzer fixed o&m in $/kW-year
