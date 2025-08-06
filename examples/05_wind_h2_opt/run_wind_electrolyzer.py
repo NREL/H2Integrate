c2a5447ee385fe82be1e0e068a3b4d667cb03e46
Jared Thomas <jaredthomas68@gmail.com>
2025-08-06 10:30:05 -0600

    add PyXDSM output to .gitignore

--- examples/05_wind_h2_opt/run_wind_electrolyzer.py
+++ examples/05_wind_h2_opt/run_wind_electrolyzer.py
@@ -1,5 +1,3 @@
-import openmdao.api as om
-
 from h2integrate.core.h2integrate_model import H2IntegrateModel
 
 
@@ -10,5 +8,3 @@ gh = H2IntegrateModel("wind_plant_electrolyzer.yaml")
 gh.run()
 
 gh.post_process()
-
-om.n2(gh.prob)
