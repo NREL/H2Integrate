284291c833e38d8d0fb6ce00ce1c36b963736fbe
Jared Thomas <jaredthomas68@gmail.com>
2025-08-25 16:45:00 -0600

    update ard example for some refactor

--- examples/xx_wind_ard/run_wind_electrolyzer.py
+++ examples/xx_wind_ard/run_wind_electrolyzer.py
@@ -2,7 +2,7 @@ from h2integrate.core.h2integrate_model import H2IntegrateModel
 
 
 # Create a GreenHEART model
-h2i_model = H2IntegrateModel("wind_plant_electrolyzer.yaml")
+h2i_model = H2IntegrateModel("./wind_plant_electrolyzer.yaml")
 
 # Run the model
 h2i_model.run()
