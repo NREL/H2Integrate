a0834cf8ad663052ea88aa989722eb33d2429683
Jared Thomas <jaredthomas68@gmail.com>
2025-08-26 09:28:48 -0600

    add resource request

--- examples/xx_wind_ard/run_wind_electrolyzer.py
+++ examples/xx_wind_ard/run_wind_electrolyzer.py
@@ -1,5 +1,17 @@
 from h2integrate.core.h2integrate_model import H2IntegrateModel
+from h2integrate.resource.get_wind_data import fetch_and_save_wind_data
 
+
+fetch_and_save_wind_data(
+    source="open-meteo",
+    year=2023,
+    height=100,
+    latitude=47.5233,
+    longitude=-92.5366,
+    turbulence_intensity=0.1,
+    elevation=439.0,
+    output="wind_resource.csv",
+)
 
 # Create a GreenHEART model
 h2i_model = H2IntegrateModel("./wind_plant_electrolyzer.yaml")
