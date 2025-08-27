752a367ff074c0c0f34635be03e1d1d8256a7e71
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-08-27 15:41:07 -0600

    finish cherry-pick

--- h2integrate/core/supported_models.py
+++ h2integrate/core/supported_models.py
@@ -1,6 +1,7 @@
 from h2integrate.resource.river import RiverResource
 from h2integrate.core.feedstocks import FeedstockCostModel, FeedstockPerformanceModel
 from h2integrate.transporters.pipe import PipePerformanceModel
+from h2integrate.core.npv_financial import NPVFinancial
 from h2integrate.transporters.cable import CablePerformanceModel
 from h2integrate.converters.steel.steel import SteelPerformanceModel, SteelCostAndFinancialModel
 from h2integrate.converters.wind.wind_plant import WindPlantCostModel, WindPlantPerformanceModel
@@ -155,6 +156,7 @@ supported_models = {
     "feedstock_cost": FeedstockCostModel,
     # Finance
     "ProFastComp": ProFastComp,
+    "NPVFinancial": NPVFinancial,
 }
 
 electricity_producing_techs = ["wind", "solar", "pv", "river", "hopp", "natural_gas_plant"]
