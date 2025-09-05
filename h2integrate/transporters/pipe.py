fc77ff9dccec7b630191b4a4bc3aea66edc4faa8
John Jasa <johnjasa11@gmail.com>
2025-09-04 18:27:38 -0600

    Added pipe test

--- h2integrate/transporters/pipe.py
+++ h2integrate/transporters/pipe.py
@@ -1,7 +1,4 @@
 import openmdao.api as om
-from attrs import field
-
-from h2integrate.core.validators import contains
 
 
 class PipePerformanceModel(om.ExplicitComponent):
@@ -9,13 +6,14 @@ class PipePerformanceModel(om.ExplicitComponent):
     Pass-through pipe with no losses.
     """
 
-    transport_item: str = field(
-        validator=contains(["hydrogen", "co2", "methanol", "ammonia", "nitrogen"])
-    )
+    def initialize(self):
+        self.options.declare(
+            "transport_item", values=["hydrogen", "co2", "methanol", "ammonia", "nitrogen"]
+        )
 
     def setup(self):
-        self.input_name = self.transport_item + "_in"
-        self.output_name = self.transport_item + "_out"
+        self.input_name = self.options["transport_item"] + "_in"
+        self.output_name = self.options["transport_item"] + "_out"
         self.add_input(
             self.input_name,
             val=0.0,
