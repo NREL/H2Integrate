16d525c464956a07f70ab600a0134830c570dbc0
jmartin4 <jonathan.martin@nrel.gov>
2025-09-05 16:51:30 -0600

    Switching HB cases

--- examples/12_ammonia_synloop/run_ammonia_synloop.py
+++ examples/12_ammonia_synloop/run_ammonia_synloop.py
@@ -1,10 +1,24 @@
+from pathlib import Path
+
+from h2integrate.tools.run_cases import mod_tech_config, load_tech_config_cases
 from h2integrate.core.h2integrate_model import H2IntegrateModel
 
 
 # Create a H2Integrate model
 model = H2IntegrateModel("12_ammonia_synloop.yaml")
 
-# Run the model
-model.run()
+# Load cases
+case_file = Path("test_inputs.csv")
+cases = load_tech_config_cases(case_file)
 
+# Modify and run the model for Haber Bosch Big
+case = cases["Haber Bosch Big"]
+model = mod_tech_config(model, case)
+model.run()
+model.post_process()
+
+# Modify and run the model for Haber Bosch Small
+case = cases["Haber Bosch Small"]
+model = mod_tech_config(model, case)
+model.run()
 model.post_process()
