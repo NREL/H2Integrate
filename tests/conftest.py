08eb8120faa493debbca0ad018bf1c7ffb517894
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-09-12 11:34:20 -0600

    updated and added pytest session start and end configs to not cause issues for users that specify resource_dir environment variable

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,3 +14,6 @@ def pytest_sessionstart(session):
     # Set a dummy API key
     os.environ["NREL_API_KEY"] = "a" * 40
     set_nrel_key_dot_env()
+
+    # Set RESOURCE_DIR to None so pulls example files from default DIR
+    os.environ.pop("RESOURCE_DIR", None)
