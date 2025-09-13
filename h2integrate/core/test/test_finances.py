fbca82468b05f53aee0848e9d05d015427d88b6c
kbrunik <kbrunik@gmail.com>
2025-09-13 08:24:01 -0500

    update tests

--- h2integrate/core/test/test_finances.py
+++ h2integrate/core/test/test_finances.py
@@ -101,7 +101,7 @@ class TestProFastComp(unittest.TestCase):
         tech_config = load_tech_yaml(example_case_dir / "tech_config.yaml")
         plant_config = load_plant_yaml(example_case_dir / "plant_config.yaml")
         driver_config = load_driver_yaml(example_case_dir / "driver_config.yaml")
-        finance_inputs = plant_config["finance_parameters"].pop("profast_model")
+        finance_inputs = plant_config["finance_parameters"]["finance_groups"].pop("profast_model")
         plant_config_filtered = {k: v for k, v in plant_config.items() if k != "finance_parameters"}
         plant_config_filtered.update({"finance_parameters": finance_inputs})
         # Run ProFastComp with loaded configs
@@ -140,11 +140,11 @@ class TestProFastComp(unittest.TestCase):
         driver_config = load_driver_yaml(example_case_dir / "driver_config.yaml")
 
         # Only include HOPP and electrolyzer in metrics
-        plant_config["finance_parameters"]["subgroups"]["electricity"]["technologies"] = [
+        plant_config["finance_parameters"]["finance_subgroups"]["electricity"]["technologies"] = [
             "hopp",
             "steel",
         ]
-        finance_inputs = plant_config["finance_parameters"].pop("profast_model")
+        finance_inputs = plant_config["finance_parameters"]["finance_groups"].pop("profast_model")
         plant_config_filtered = {k: v for k, v in plant_config.items() if k != "finance_parameters"}
         plant_config_filtered.update({"finance_parameters": finance_inputs})
 
