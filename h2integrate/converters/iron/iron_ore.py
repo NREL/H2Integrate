ea24ab91ddc2e4743b1332959342893b8f3984fd
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-09-25 15:21:53 -0600

    added iron ore cost and performance model

---
+++ h2integrate/converters/iron/iron_ore.py
@@ -0,0 +1,226 @@
+import numpy as np
+import openmdao.api as om
+from attrs import field, define
+
+from h2integrate.core.utilities import BaseConfig, merge_shared_inputs
+from h2integrate.core.validators import contains, range_val
+from h2integrate.core.model_baseclasses import CostModelBaseClass
+from h2integrate.tools.inflation.inflate import inflate_cpi
+from h2integrate.simulation.technologies.iron.iron import (
+    IronCostModelConfig,
+    IronPerformanceModelConfig,
+    IronPerformanceModelOutputs,
+    run_iron_cost_model,
+    run_size_iron_plant_performance,
+)
+from h2integrate.simulation.technologies.iron.martin_ore.variable_om_cost import (
+    martin_ore_variable_om_cost,
+)
+from h2integrate.simulation.technologies.iron.rosner_ore.variable_om_cost import (
+    rosner_ore_variable_om_cost,
+)
+
+
+@define
+class IronOreBaseConfig(BaseConfig):
+    # site['name']
+    mine: str = field(
+        validator=contains(["Hibbing", "Northshore", "United", "Minorca", "Tilden"])
+    )  # ore
+
+    # product_selection
+    taconite_pellet_type: str = field(
+        converter=(str.lower, str.strip), validator=contains(["std", "drg"])
+    )  # ore
+
+    # performance['input_capacity_factor_estimate']
+    # ore_cf_estimate: float = field(default=0.9, validator=range_val(0, 1))  # ore
+
+    model_name: str = field(default="martine_ore")  # only option at the moment
+    model_fp: str = field(default="")
+    inputs_fp: str = field(default="")
+    coeffs_fp: str = field(default="")
+    refit_coeffs: bool = field(default=False)
+
+    def make_model_dict(self):
+        keys = ["model_fp", "inputs_fp", "coeffs_fp", "refit_coeffs"]
+        d = self.as_dict()
+        model_dict = {k: v for k, v in d.items() if k in keys}
+        model_dict.update({"name": self.model_name})
+        return model_dict
+
+    def make_site_dict(self):
+        return {"name": self.mine}
+
+
+@define
+class IronOrePerformanceConfig(IronOreBaseConfig):
+    ore_cf_estimate: float = field(default=0.9, validator=range_val(0, 1))  # ore
+
+    def make_model_dict(self):
+        keys = ["model_fp", "inputs_fp", "coeffs_fp", "refit_coeffs"]
+        d = self.as_dict()
+        model_dict = {k: v for k, v in d.items() if k in keys}
+        model_dict.update({"name": self.model_name})
+        return model_dict
+
+
+class IronOrePerformanceComponent(om.ExplicitComponent):
+    def initialize(self):
+        self.options.declare("driver_config", types=dict)
+        self.options.declare("plant_config", types=dict)
+        self.options.declare("tech_config", types=dict)
+
+    def setup(self):
+        self.config = IronOrePerformanceConfig.from_dict(
+            merge_shared_inputs(self.options["tech_config"]["model_inputs"], "performance"),
+            strict=False,
+        )
+        self.add_discrete_output("iron_ore_performance")
+        self.add_output("total_iron_ore_produced", val=0.0, units="t/year")
+
+    def compute(self, inputs, outputs, discrete_inputs, discrete_outputs):
+        # iron_ore_site = {'name': self.config.mine}
+        ore_performance_inputs = {"input_capacity_factor_estimate": self.config.ore_cf_estimate}
+        ore_model_inputs = self.config.make_model_dict()
+        iron_ore_site = self.config.make_site_dict()
+        performance_config = IronPerformanceModelConfig(
+            product_selection=f"{self.config.taconite_pellet_type}_taconite_pellets",
+            site=iron_ore_site,
+            model=ore_model_inputs,
+            params=ore_performance_inputs,
+        )
+        iron_ore_performance = run_size_iron_plant_performance(performance_config)
+        # wltpy = wet long tons per year
+        ore_produced_wltpy = iron_ore_performance.performances_df.set_index("Name").loc[
+            "Ore pellets produced"
+        ][self.config.mine]
+        # ore_produced_wltpy = iron_ore_cost.costs_df.loc["Ore pellets produced"]
+        ore_produced_wmtpy = ore_produced_wltpy * 1.016047  # wmtpy = wet metric tonnes per year
+        ore_produced_mtpy = ore_produced_wmtpy * 0.98  # mtpy = dry metric tonnes per year
+        discrete_outputs["iron_ore_performance"] = iron_ore_performance.performances_df
+        outputs["total_iron_ore_produced"] = ore_produced_mtpy
+
+
+@define
+class IronOreCostConfig(IronOreBaseConfig):
+    LCOE: float = field(kw_only=True)  # $/MWh
+    LCOH: float = field(kw_only=True)  # $/kg
+    varom_model_name: str = field(
+        default="martine_ore", validator=contains["martin_ore", "rosner_ore"]
+    )
+    operational_year: int = field(converter=int, kw_only=True)  # also used in finance
+    installation_years: int | float = field(kw_only=True)  # 3 #also used in finance
+    plant_life: int = field(converter=int, kw_only=True)  # also used in finance
+    cost_year: int = field(converter=int, kw_only=True)
+
+    def make_model_dict(self):
+        keys = ["model_fp", "inputs_fp", "coeffs_fp", "refit_coeffs"]
+        d = self.as_dict()
+        model_dict = {k: v for k, v in d.items() if k in keys}
+        model_dict.update({"name": self.model_name})
+        return model_dict
+
+    def make_cost_dict(self):
+        keys = ["operational_year", "installation_years", "plant_life"]
+        d = self.as_dict()
+        cost_dict = {k: v for k, v in d.items() if k in keys}
+        return cost_dict
+
+
+class IronOreCostComponent(CostModelBaseClass):
+    def setup(self):
+        self.target_dollar_year = self.options["plant_config"]["finance_parameters"][
+            "cost_adjustment_parameters"
+        ]["target_dollar_year"]
+        self.plant_life = self.options["plant_config"]["plant"]["plant_life"]
+
+        config_dict = merge_shared_inputs(self.options["tech_config"]["model_inputs"], "cost")
+        config_dict.update({"cost_year": self.target_dollar_year})
+        config_dict.update({"plant_life": self.plant_life})
+
+        self.config = IronOreCostConfig.from_dict(config_dict, strict=False)
+
+        super().setup()
+        self.add_input("LCOE", val=self.config.LCOE, units="USD/MW/h")
+        self.add_input("LCOH", val=self.config.LCOH, units="USD/kg")
+        self.add_discrete_input("iron_ore_performance")
+        # self.add_discrete_output("iron_ore_cost")
+
+    def compute(self, inputs, outputs, discrete_inputs, discrete_outputs):
+        ore_performance = IronPerformanceModelOutputs(discrete_inputs["iron_ore_performance"])
+
+        ore_cost_inputs = {
+            "lcoe": inputs["LCOE"][0] / 1e3,
+            "lcoh": inputs["LCOH"][0] / 1e3,
+        }
+        cost_dict = self.config.make_cost_dict()
+        ore_cost_inputs.update(cost_dict)
+
+        ore_model_inputs = self.config.make_model_dict()
+        iron_ore_site = self.config.make_site_dict()
+        cost_config = IronCostModelConfig(
+            product_selection=f"{self.config.taconite_pellet_type}_taconite_pellets",
+            site=iron_ore_site,
+            model=ore_model_inputs,
+            params=ore_cost_inputs,
+            performance=ore_performance,
+        )
+        iron_ore_cost = run_iron_cost_model(cost_config)
+        # discrete_outputs['iron_ore_cost'] = iron_ore_cost.costs_df
+
+        # Now taking some stuff from finance
+        cost_df = iron_ore_cost.costs_df.set_index("Name")
+        cost_ds = cost_df.loc[:, self.config.mine]
+        cost_names = cost_df.index.values
+        cost_types = cost_df.loc[:, "Type"].values
+        cost_units = cost_df.loc[:, "Unit"].values
+
+        capex = 0
+        fixed_om = 0
+        variable_om = 0
+        capital_idxs = np.where(cost_types == "capital")[0]
+        for idx in capital_idxs:
+            cost_names[idx]
+            unit = cost_units[idx]  # Units for capital costs should be "<YYYY> $""
+            source_year = int(unit[:4])
+            source_year_cost = cost_ds.iloc[idx]
+            cost = inflate_cpi(source_year_cost, source_year, self.config.cost_year)
+            capex += cost
+
+        # Add fixed opex costs
+        fixed_idxs = np.where(cost_types == "fixed opex")[0]
+        for idx in fixed_idxs:
+            cost_names[idx]
+            unit = cost_units[idx]  # Units for fixed opex costs should be "<YYYY> $ per year"
+            source_year = int(unit[:4])
+            source_year_cost = cost_ds.iloc[idx]
+            fixed_cost = inflate_cpi(source_year_cost, source_year, self.config.cost_year)
+            fixed_om += fixed_cost
+        # NOTE: why are we double counting this cost?
+        fixed_om * 6 / 12  # TODO: output installation cost?
+
+        var_idxs = np.where(cost_types == "variable opex")[0]
+        for idx in var_idxs:
+            cost_names[idx]
+            unit = cost_units[idx]  # Should be "<YYYY> $ per <unit plant output>"
+            source_year = int(unit[:4])
+            source_year_cost = cost_ds.iloc[idx]
+            cost = inflate_cpi(source_year_cost, source_year, self.config.cost_year)
+            variable_om += cost
+
+        # below is for rosner finance model
+        analysis_start = self.config.operational_year - self.config.installation_years
+        var_om_td = 0
+        if "rosner" in self.config.varom_model_name:
+            var_om_td = rosner_ore_variable_om_cost()
+        else:
+            #  'martin' in self.config.varom_model_name:
+            var_om_td = martin_ore_variable_om_cost(
+                self.config.mine, cost_df, analysis_start, self.config.cost_year, self.plant_life
+            )
+        var_om_td += cost
+
+        outputs["CapEx"] = capex
+        outputs["OpEx"] = fixed_om
+        outputs["VarOpEx"] = var_om_td
---
+++ h2integrate/converters/iron/test/test_iron_ore.py
@@ -0,0 +1,142 @@
+import pytest
+import openmdao.api as om
+from pytest import fixture
+
+from h2integrate import EXAMPLE_DIR
+from h2integrate.core.inputs.validation import load_driver_yaml
+from h2integrate.converters.iron.iron_ore import IronOreCostComponent, IronOrePerformanceComponent
+
+
+@fixture
+def iron_ore_config_martin_om():
+    shared_params = {
+        "mine": "Northshore",
+        "taconite_pellet_type": "drg",
+    }
+    performance_params = {"ore_cf_estimate": 0.9, "model_name": "martin_ore"}
+    cost_params = {
+        "LCOE": 58.02,
+        "LCOH": 7.10,
+        "model_name": "martin_ore",
+        "varom_model_name": "martin_ore",
+        "installation_years": 3,
+        "operational_year": 2035,
+        # 'plant_life': 30,
+    }
+
+    tech_config = {
+        "model_inputs": {
+            "cost_parameters": cost_params,
+            "performance_parameters": performance_params,
+            "shared_parameters": shared_params,
+        }
+    }
+    return tech_config
+
+
+@fixture
+def iron_ore_config_rosner_om():
+    shared_params = {
+        "mine": "Northshore",
+        "taconite_pellet_type": "drg",
+    }
+    performance_params = {"ore_cf_estimate": 0.9, "model_name": "martin_ore"}
+    cost_params = {
+        "LCOE": 58.02,
+        "LCOH": 7.10,
+        "model_name": "martin_ore",
+        "varom_model_name": "rosner_ore",
+        "installation_years": 3,
+        "operational_year": 2035,
+        # 'plant_life': 30,
+    }
+
+    tech_config = {
+        "model_inputs": {
+            "cost_parameters": cost_params,
+            "performance_parameters": performance_params,
+            "shared_parameters": shared_params,
+        }
+    }
+    return tech_config
+
+
+@fixture
+def plant_config():
+    plant_config = {
+        "plant": {
+            "plant_life": 30,
+        },
+        "finance_parameters": {
+            "cost_adjustment_parameters": {
+                "cost_year_adjustment_inflation": 0.025,
+                "target_dollar_year": 2020,
+            }
+        },
+    }
+    return plant_config
+
+
+@fixture
+def driver_config():
+    driver_config = load_driver_yaml(EXAMPLE_DIR / "20_iron_mn_to_il" / "driver_config.yaml")
+    return driver_config
+
+
+# baseline case
+def test_baseline_iron_ore_martin(plant_config, driver_config, iron_ore_config_martin_om, subtests):
+    martin_ore_capex = 1221599018.626594
+    martin_ore_var_om = 97.76558025830258
+    martin_ore_fixed_om = 0.0
+    prob = om.Problem()
+    iron_ore_perf = IronOrePerformanceComponent(
+        plant_config=plant_config,
+        tech_config=iron_ore_config_martin_om,
+        driver_config=driver_config,
+    )
+
+    iron_ore_cost = IronOreCostComponent(
+        plant_config=plant_config,
+        tech_config=iron_ore_config_martin_om,
+        driver_config=driver_config,
+    )
+
+    prob.model.add_subsystem("ore_perf", iron_ore_cost, promotes=["*"])
+    prob.model.add_subsystem("ore_cost", iron_ore_perf, promotes=["*"])
+    prob.setup()
+    prob.run_model()
+    with subtests.test("CapEx"):
+        assert pytest.approx(prob.get_val("iron_cost.CapEx")[0], rel=1e-6) == martin_ore_capex
+    with subtests.test("OpEx"):
+        assert pytest.approx(prob.get_val("iron_cost.OpEx")[0], rel=1e-6) == martin_ore_fixed_om
+    with subtests.test("VarOpEx"):
+        assert pytest.approx(prob.get_val("iron_cost.VarOpEx")[0], rel=1e-6) == martin_ore_var_om
+
+
+def test_baseline_iron_ore_rosner(plant_config, driver_config, iron_ore_config_rosner_om, subtests):
+    rosner_ore_capex = 1221599018.626594
+    rosner_ore_var_om = 97.76558025830258
+    rosner_ore_fixed_om = 0.0
+    prob = om.Problem()
+    iron_ore_perf = IronOrePerformanceComponent(
+        plant_config=plant_config,
+        tech_config=iron_ore_config_rosner_om,
+        driver_config=driver_config,
+    )
+
+    iron_ore_cost = IronOreCostComponent(
+        plant_config=plant_config,
+        tech_config=iron_ore_config_rosner_om,
+        driver_config=driver_config,
+    )
+
+    prob.model.add_subsystem("ore_perf", iron_ore_cost, promotes=["*"])
+    prob.model.add_subsystem("ore_cost", iron_ore_perf, promotes=["*"])
+    prob.setup()
+    prob.run_model()
+    with subtests.test("CapEx"):
+        assert pytest.approx(prob.get_val("iron_cost.CapEx")[0], rel=1e-6) == rosner_ore_capex
+    with subtests.test("OpEx"):
+        assert pytest.approx(prob.get_val("iron_cost.OpEx")[0], rel=1e-6) == rosner_ore_fixed_om
+    with subtests.test("VarOpEx"):
+        assert pytest.approx(prob.get_val("iron_cost.VarOpEx")[0], rel=1e-6) == rosner_ore_var_om
