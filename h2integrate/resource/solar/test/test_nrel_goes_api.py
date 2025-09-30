89f29475815f95c9b605c0835cf1d082ba67c14a
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-09-30 09:46:34 -0600

    added tests for other goes solar resource models

--- h2integrate/resource/solar/test/test_nrel_goes_api.py
+++ h2integrate/resource/solar/test/test_nrel_goes_api.py
@@ -5,6 +5,9 @@ from pytest import fixture
 
 from h2integrate import RESOURCE_DEFAULT_DIR
 from h2integrate.resource.solar.nrel_developer_goes_api_models import (
+    GOESTMYNRELDeveloperAPISolarResource,
+    GOESConusNRELDeveloperAPISolarResource,
+    GOESFullDiscNRELDeveloperAPISolarResource,
     GOESAggregatedNRELDeveloperAPISolarResource,
 )
 
@@ -42,7 +45,7 @@ def site_config():
     return site
 
 
-def test_solar_resource_loaded_from_default_dir(plant_simulation_utc_start, site_config, subtests):
+def test_goes_aggregated_loaded_from_default_dir(plant_simulation_utc_start, site_config, subtests):
     plant_config = {
         "site": site_config,
         "plant": plant_simulation_utc_start,
@@ -50,6 +53,139 @@ def test_solar_resource_loaded_from_default_dir(plant_simulation_utc_start, site
 
     prob = om.Problem()
     comp = GOESAggregatedNRELDeveloperAPISolarResource(
+        plant_config=plant_config,
+        resource_config=plant_config["site"]["resources"]["solar_resource"]["resource_parameters"],
+        driver_config={},
+    )
+    prob.model.add_subsystem("resource", comp)
+    prob.setup()
+    prob.run_model()
+    goes_data = prob.get_val("resource.solar_resource_data")
+
+    solar_dir = RESOURCE_DEFAULT_DIR / "solar"
+    with subtests.test("filepath for data was found where expected"):
+        assert Path(goes_data["filepath"]).exists()
+        assert Path(goes_data["filepath"]).parent == solar_dir
+
+    data_keys = [
+        "ghi",
+        "dhi",
+        "dni",
+        "temperature",
+        "pressure",
+        "dew_point",
+        "wind_speed",
+        "wind_direction",
+    ]
+    with subtests.test("resource data is 8760 in length"):
+        assert all(len(goes_data[k]) == 8760 for k in data_keys)
+
+
+def test_goes_conus_loaded_from_default_dir(plant_simulation_utc_start, site_config, subtests):
+    plant_config = {
+        "site": site_config,
+        "plant": plant_simulation_utc_start,
+    }
+
+    filename = "34.22_-102.75_2012_goes_aggregated_v4_60min_utc_tz.csv"
+    plant_config["site"]["resources"]["solar_resource"]["resource_parameters"].update(
+        {"resource_filename": filename}
+    )
+    plant_config["site"]["resources"]["solar_resource"]["resource_parameters"]["resource_year"] = (
+        2020
+    )
+
+    prob = om.Problem()
+    comp = GOESConusNRELDeveloperAPISolarResource(
+        plant_config=plant_config,
+        resource_config=plant_config["site"]["resources"]["solar_resource"]["resource_parameters"],
+        driver_config={},
+    )
+    prob.model.add_subsystem("resource", comp)
+    prob.setup()
+    prob.run_model()
+    goes_data = prob.get_val("resource.solar_resource_data")
+
+    solar_dir = RESOURCE_DEFAULT_DIR / "solar"
+    with subtests.test("filepath for data was found where expected"):
+        assert Path(goes_data["filepath"]).exists()
+        assert Path(goes_data["filepath"]).parent == solar_dir
+
+    data_keys = [
+        "ghi",
+        "dhi",
+        "dni",
+        "temperature",
+        "pressure",
+        "dew_point",
+        "wind_speed",
+        "wind_direction",
+    ]
+    with subtests.test("resource data is 8760 in length"):
+        assert all(len(goes_data[k]) == 8760 for k in data_keys)
+
+
+def test_goes_fulldisc_loaded_from_default_dir(plant_simulation_utc_start, site_config, subtests):
+    plant_config = {
+        "site": site_config,
+        "plant": plant_simulation_utc_start,
+    }
+
+    filename = "34.22_-102.75_2012_goes_aggregated_v4_60min_utc_tz.csv"
+    plant_config["site"]["resources"]["solar_resource"]["resource_parameters"].update(
+        {"resource_filename": filename}
+    )
+    plant_config["site"]["resources"]["solar_resource"]["resource_parameters"]["resource_year"] = (
+        2020
+    )
+
+    prob = om.Problem()
+    comp = GOESFullDiscNRELDeveloperAPISolarResource(
+        plant_config=plant_config,
+        resource_config=plant_config["site"]["resources"]["solar_resource"]["resource_parameters"],
+        driver_config={},
+    )
+    prob.model.add_subsystem("resource", comp)
+    prob.setup()
+    prob.run_model()
+    goes_data = prob.get_val("resource.solar_resource_data")
+
+    solar_dir = RESOURCE_DEFAULT_DIR / "solar"
+    with subtests.test("filepath for data was found where expected"):
+        assert Path(goes_data["filepath"]).exists()
+        assert Path(goes_data["filepath"]).parent == solar_dir
+
+    data_keys = [
+        "ghi",
+        "dhi",
+        "dni",
+        "temperature",
+        "pressure",
+        "dew_point",
+        "wind_speed",
+        "wind_direction",
+    ]
+    with subtests.test("resource data is 8760 in length"):
+        assert all(len(goes_data[k]) == 8760 for k in data_keys)
+
+
+def test_goes_tmy_loaded_from_default_dir(plant_simulation_utc_start, site_config, subtests):
+    plant_config = {
+        "site": site_config,
+        "plant": plant_simulation_utc_start,
+    }
+
+    filename = "34.22_-102.75_2012_goes_aggregated_v4_60min_utc_tz.csv"
+    plant_config["site"]["resources"]["solar_resource"]["resource_parameters"].update(
+        {"resource_filename": filename}
+    )
+
+    plant_config["site"]["resources"]["solar_resource"]["resource_parameters"]["resource_year"] = (
+        "tmy-2022"
+    )
+
+    prob = om.Problem()
+    comp = GOESTMYNRELDeveloperAPISolarResource(
         plant_config=plant_config,
         resource_config=plant_config["site"]["resources"]["solar_resource"]["resource_parameters"],
         driver_config={},
