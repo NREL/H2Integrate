a4d5db543cb058ea439670f1374b8e96d95a9b88
Jared Thomas <jaredthomas68@gmail.com>
2025-10-08 09:44:38 -0600

    remove h2 storage rules

--- h2integrate/control/control_rules/storage/h2_storage.py
+++
@@ -1,7 +0,0 @@
-from h2integrate.control.control_rules.storage.pyomo_storage_rule_baseclass import (
-    PyomoRuleStorageBaseclass,
-)
-
-
-class PyomoDispatchH2Storage(PyomoRuleStorageBaseclass):
-    pass
