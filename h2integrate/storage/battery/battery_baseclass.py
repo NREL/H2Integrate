d2577772c48e89e585224530bbb89b66ebdcbada
Jared Thomas <jaredthomas68@users.noreply.github.com>
2025-10-16 17:58:34 -0600

    Update h2integrate/storage/battery/battery_baseclass.py
    Co-authored-by: John Jasa <john.jasa@nrel.gov>

--- h2integrate/storage/battery/battery_baseclass.py
+++ h2integrate/storage/battery/battery_baseclass.py
@@ -37,7 +37,7 @@ class BatteryPerformanceBaseClass(om.ExplicitComponent):
             val=0.0,
             copy_shape="electricity_in",
             units="kW",
-            desc="Electricty output from Battery only",
+            desc="Electricity output from Battery only",
         )
 
     def compute(self, inputs, outputs):
