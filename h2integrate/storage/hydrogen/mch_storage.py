4cf8a43826e3f3cae6666f932969d75963c8ccbb
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-10-30 11:18:33 -0600

    added tests for mch wrapper but failing

--- h2integrate/storage/hydrogen/mch_storage.py
+++ h2integrate/storage/hydrogen/mch_storage.py
@@ -3,20 +3,21 @@ from attrs import field, define
 from openmdao.utils import units
 
 from h2integrate.core.utilities import BaseConfig, merge_shared_inputs
-from h2integrate.core.validators import contains
+from h2integrate.core.validators import contains, range_val
 from h2integrate.core.model_baseclasses import CostModelBaseClass
-from h2integrate.simulation.technologies.hydrogen.h2_storage.mch.mch_cost import MCHStorage
 
 
 @define
 class MCHTOLStorageCostModelConfig(BaseConfig):
     max_capacity: float = field()
     max_charge_rate: float = field()
-    compressor_output_pressure: float = field(default=100)  # bar
+    max_discharge_rate: float = field()
+    charge_equals_discharge: bool = field(default=True)
 
     commodity_name: str = field(default="hydrogen")
     commodity_units: str = field(default="kg/h", validator=contains(["kg/h", "g/h", "t/h"]))
 
+    storage_efficiency: float = field(default=0.9984, validator=range_val(0, 1))
     cost_year: int = field(default=2024, converter=int, validator=contains([2024]))
 
 
@@ -26,7 +27,7 @@ class MCHTOLStorageCostModel(CostModelBaseClass):
 
     def setup(self):
         self.config = MCHTOLStorageCostModelConfig.from_dict(
-            merge_shared_inputs(self.options["tech_config"]["model_inputs"], "performance"),
+            merge_shared_inputs(self.options["tech_config"]["model_inputs"], "cost"),
             strict=False,
         )
 
@@ -38,6 +39,21 @@ class MCHTOLStorageCostModel(CostModelBaseClass):
             units=f"{self.config.commodity_units}",
             desc="Hydrogen storage charge rate",
         )
+
+        if self.config.charge_equals_discharge:
+            self.add_input(
+                "max_discharge_rate",
+                val=self.config.max_charge_rate,
+                units=f"{self.config.commodity_units}",
+                desc="Hydrogen storage discharge rate",
+            )
+        else:
+            self.add_input(
+                "max_discharge_rate",
+                val=self.config.max_discharge_rate,
+                units=f"{self.config.commodity_units}",
+                desc="Hydrogen storage discharge rate",
+            )
 
         self.add_input(
             "max_capacity",
@@ -54,50 +70,93 @@ class MCHTOLStorageCostModel(CostModelBaseClass):
             shape=self.n_timesteps,
             desc="Hydrogen state of charge timeseries for storage",
         )
-        self.add_input(
-            "hydrogen_out",
-            val=0.0,
-            shape=self.n_timesteps,
-            units=f"{self.config.commodity_units}",
-            desc="Hydrogen output timeseries from plant after storage including \
-            stored and pass-through commodity amounts up to the demand amount",
-        )
+
+    def calc_cost_value(self, b0, b1, b2, b3, b4):
+        """
+        Calculate the value of the cost function for the given coefficients.
+
+        Args:
+            b0 (float): Coefficient representing the base cost.
+            b1 (float): Coefficient for the Hc (hydrogenation capacity) term.
+            b2 (float): Coefficient for the Dc (dehydrogenation capacity) term.
+            b3 (float): Coefficient for the Ms (maximum storage) term.
+            b4 (float): Coefficient for the As (annual hydrogen into storage) term.
+        Returns:
+            float: The calculated cost value based on the provided coefficients and attributes.
+
+        """
+        return b0 + (b1 * self.Hc) + (b2 * self.Dc) + (b3 * self.Ms) + b4 * self.As
 
     def compute(self, inputs, outputs, discrete_inputs, discrete_outputs):
-        # convert capacity to kg
-        max_capacity_kg = units.convert_units(
-            inputs["max_capacity"], f"({self.config.commodity_units})*h", "kg"
+        # convert charge rate to kg/d
+        storage_max_fill_rate_tpd = units.convert_units(
+            inputs["max_charge_rate"], f"{self.config.commodity_units}", "t/d"
         )
 
         # convert charge rate to kg/d
-        storage_max_fill_rate = units.convert_units(
-            inputs["max_charge_rate"], f"{self.config.commodity_units}", "kg/h"
-        )
+        if self.config.charge_equals_discharge:
+            storage_max_empty_rate_tpd = units.convert_units(
+                inputs["max_charge_rate"], f"{self.config.commodity_units}", "t/d"
+            )
+        else:
+            storage_max_empty_rate_tpd = units.convert_units(
+                inputs["max_discharge_rate"], f"{self.config.commodity_units}", "t/d"
+            )
 
-        hydrogen_storage_soc = units.convert_units(
+        hydrogen_storage_soc_kg = units.convert_units(
             inputs["hydrogen_soc"] * inputs["max_capacity"],
             f"({self.config.commodity_units})*h",
             "kg",
         )
 
-        hydrogen_from_storage_kg = units.convert_units(
-            inputs["hydrogen_out"], f"{self.config.commodity_units}", "kg/h"
-        )
-
-        hydrogen_demand_kgphr = np.mean(hydrogen_from_storage_kg)
-
-        h2_charge_discharge = np.diff(hydrogen_storage_soc, prepend=False)
+        h2_charge_discharge = np.diff(hydrogen_storage_soc_kg, prepend=False)
         h2_charged_idx = np.argwhere(h2_charge_discharge > 0).flatten()
-        annual_h2_stored = sum([h2_charge_discharge[i] for i in h2_charged_idx])
+        annual_h2_stored_kg = sum([h2_charge_discharge[i] for i in h2_charged_idx])
 
-        h2_storage = MCHStorage(
-            max_H2_production_kg_pr_hr=storage_max_fill_rate[0],
-            hydrogen_storage_capacity_kg=max_capacity_kg[0],
-            hydrogen_demand_kg_pr_hr=hydrogen_demand_kgphr,
-            annual_hydrogen_stored_kg_pr_yr=annual_h2_stored,
+        annual_h2_stored_tpy = units.convert_units(
+            annual_h2_stored_kg,
+            "kg/yr",
+            "t/yr",
         )
-        h2_storage_costs = h2_storage.run_costs()
 
-        outputs["CapEx"] = h2_storage_costs["mch_capex"]
-        outputs["OpEx"] = h2_storage_costs["mch_opex"]
-        outputs["VarOpEx"] = h2_storage_costs["mch_variable_om"]
+        h2_storage_capacity_tons = units.convert_units(
+            inputs["max_capacity"], f"({self.config.commodity_units})*h", "t"
+        )
+        # Equation (3): DC = P_avg
+        self.Dc = storage_max_fill_rate_tpd[0]
+
+        # Equation (2): HC = P_nameplate - P_avg
+        # P_nameplate = self.max_H2_production_kg_pr_hr * 24 / 1e3
+        self.Hc = storage_max_empty_rate_tpd[0]
+
+        # Equation (1): AS = sum(curtailed_h2)
+        self.As = annual_h2_stored_tpy  # tons/year
+
+        # Defined in paragraph between Equation (2) and (3)
+        self.Ms = h2_storage_capacity_tons[0]
+
+        # overnight capital cost coefficients
+        occ_coeff = (54706639.43, 147074.25, 588779.05, 20825.39, 10.31)
+
+        #: fixed O&M cost coefficients
+        foc_coeff = (3419384.73, 3542.79, 13827.02, 61.22, 0.0)
+
+        #: variable O&M cost coefficients
+        voc_coeff = (711326.78, 1698.76, 6844.86, 36.04, 376.31)
+
+        outputs["CapEx"] = self.calc_cost_value(*occ_coeff)
+        outputs["OpEx"] = self.calc_cost_value(*foc_coeff)
+        outputs["VarOpEx"] = self.calc_cost_value(*voc_coeff)
+
+        # h2_storage = MCHStorage(
+        #     max_H2_production_kg_pr_hr=storage_max_fill_rate[0],
+        #     hydrogen_storage_capacity_kg=max_capacity_kg[0],
+        #     hydrogen_demand_kg_pr_hr=hydrogen_demand_kgphr,
+        #     annual_hydrogen_stored_kg_pr_yr=annual_h2_stored,
+        # )
+
+        # h2_storage_costs = h2_storage.run_costs()
+
+        # outputs["CapEx"] = h2_storage_costs["mch_capex"]
+        # outputs["OpEx"] = h2_storage_costs["mch_opex"]
+        # outputs["VarOpEx"] = h2_storage_costs["mch_variable_om"]
--- h2integrate/storage/hydrogen/test/test_tol_mch_storage.py
+++ h2integrate/storage/hydrogen/test/test_tol_mch_storage.py
@@ -1,5 +1,9 @@
+import numpy as np
+import pytest
+import openmdao.api as om
 from pytest import approx, fixture
 
+from h2integrate.storage.hydrogen.mch_storage import MCHTOLStorageCostModel
 from h2integrate.simulation.technologies.hydrogen.h2_storage.mch.mch_cost import MCHStorage
 
 
@@ -114,3 +118,111 @@ def test_run_lcos(tol_mch_storage, subtests):
         assert lcos_est == approx(2.05, rel=max_cost_error_rel)
     with subtests.test("lcos equation from costs"):
         assert lcos_est_from_costs == approx(2.05, rel=max_cost_error_rel)
+
+
+@fixture
+def plant_config():
+    plant_config_dict = {
+        "plant": {
+            "plant_life": 30,
+            "simulation": {
+                "n_timesteps": 8760,  # Default number of timesteps for the simulation
+            },
+        },
+    }
+    return plant_config_dict
+
+
+def test_mch_wrapper(plant_config, subtests):
+    # Ran Example 1 with MCH storage
+    # Annual H2 Stored: 17878378.49459929
+    Dc_tpd = 304
+    Hc_tpd = 304
+    As_tpy = 35000
+    Ms_tpy = 16200
+
+    toc_actual = 639375591
+    foc_actual = 10239180
+    voc_actual = 17332229
+
+    max_cost_error_rel = 0.06
+
+    tech_config_dict = {
+        "model_inputs": {
+            "shared_parameters": {
+                "max_capacity": Ms_tpy * 1e3,
+                "max_charge_rate": (Hc_tpd + Dc_tpd) * 1e3 / 24,
+                "max_discharge_rate": Dc_tpd * 1e3 / 24,  # 10775.824040553021,
+                "charge_equals_discharge": False,
+            }
+        }
+    }
+
+    target_annual_h2_stored = As_tpy * 1e3
+    c_max = Ms_tpy * 1e3
+    n_fills = target_annual_h2_stored / c_max
+    fill_ratio_per_hr = n_fills / (8760 / 2)
+    fill_per_hr = fill_ratio_per_hr * c_max
+    soc = np.tile([fill_per_hr / c_max, 0], 4380)
+    prob = om.Problem()
+    comp = MCHTOLStorageCostModel(
+        plant_config=plant_config,
+        tech_config=tech_config_dict,
+        driver_config={},
+    )
+    prob.model.add_subsystem("sys", comp)
+    prob.setup()
+    prob.set_val("sys.hydrogen_soc", soc)
+    prob.run_model()
+
+    with subtests.test("CapEx"):
+        assert pytest.approx(prob.get_val("sys.CapEx")[0], rel=max_cost_error_rel) == toc_actual
+    with subtests.test("OpEx"):
+        assert pytest.approx(prob.get_val("sys.OpEx")[0], rel=max_cost_error_rel) == foc_actual
+    with subtests.test("VarOpEx"):
+        assert (
+            pytest.approx(np.sum(prob.get_val("sys.VarOpEx")), rel=max_cost_error_rel) == voc_actual
+        )
+    with subtests.test("Cost year"):
+        assert prob.get_val("sys.cost_year") == 2024
+
+
+def test_mch_wrapper_ex1(plant_config, subtests):
+    # Ran Example 1 with MCH storage
+    # Annual H2 Stored: 17878378.49459929
+    tech_config_dict = {
+        "model_inputs": {
+            "shared_parameters": {
+                "max_capacity": 2081385.9326778147,
+                "max_charge_rate": 14118.146788766157,
+                "max_discharge_rate": 3342.322748213135,  # 10775.824040553021,
+                "charge_equals_discharge": False,
+            }
+        }
+    }
+
+    target_annual_h2_stored = 17878378.49459929
+    c_max = 2081385.9326778147
+    n_fills = target_annual_h2_stored / c_max
+    fill_ratio_per_hr = n_fills / (8760 / 2)
+    fill_per_hr = fill_ratio_per_hr * c_max
+    soc = np.tile([fill_per_hr / c_max, 0], 4380)
+    prob = om.Problem()
+    comp = MCHTOLStorageCostModel(
+        plant_config=plant_config,
+        tech_config=tech_config_dict,
+        driver_config={},
+    )
+    prob.model.add_subsystem("sys", comp)
+    prob.setup()
+    prob.set_val("sys.hydrogen_soc", soc)
+    prob.run_model()
+
+    with subtests.test("CapEx"):
+        assert pytest.approx(prob.get_val("sys.CapEx")[0], rel=1e-6) == 2.62304217 * 1e8
+    with subtests.test("OpEx"):
+        assert pytest.approx(prob.get_val("sys.OpEx")[0], rel=1e-6) == 7406935.548022923
+    with subtests.test("VarOpEx"):
+        assert pytest.approx(np.sum(prob.get_val("sys.VarOpEx")), rel=1e-6) == 9420636.00753175 * 30
+    with subtests.test("Cost year"):
+        assert prob.get_val("sys.cost_year") == 2024
