679190ef0a395d6dab193d5377152c0ceb101b8c
elenya-grant <116225007+elenya-grant@users.noreply.github.com>
2025-11-12 12:50:00 -0700

    updated convert_sql_to_csv_summary to work if run in parallel

--- h2integrate/postprocess/sql_to_csv.py
+++ h2integrate/postprocess/sql_to_csv.py
@@ -74,12 +74,20 @@ def summarize_case(case, return_units=False):
 
 
 def convert_sql_to_csv_summary(sql_fpath: Path | str, save_to_file: bool = True):
-    """Summarize data from a sql recorder file to a csv file.
+    """Summarize scalar data from a sql recorder file (if ran in serial) or set of sql
+    recorder files (if ran in parallel) to a DataFrame and same to csv file if
+    `save_to_file` is True.
+
+    The first columns of the output DataFrame are the design variables.
+    Column names are formatted as:
+
+    - "{promoted variable name} ({units})" for continous variables
+    - "{promoted variable name}" for discrete variables.
 
     Args:
         sql_fpath (Path | str): Filepath to sql recorder file.
         save_to_file (bool, optional): Whether to save the summary csv file to the same
-            folder as the sql file. Defaults to True.
+            folder as the sql file(s). Defaults to True.
 
     Raises:
         FileNotFoundError: if sql_fpath does not point to a valid filename.
@@ -87,25 +95,37 @@ def convert_sql_to_csv_summary(sql_fpath: Path | str, save_to_file: bool = True)
     Returns:
         pd.DataFrame: summary of scalar results from the sql file.
     """
+    sql_fpath = Path(sql_fpath)
+
+    sql_files = list(Path(sql_fpath.parent).glob(f"{sql_fpath.name}*"))
 
     # check that file exists
-    if not Path(sql_fpath).is_file():
+    if len(sql_files) == 0:
         raise FileNotFoundError(f"{sql_fpath} file does not exist.")
 
-    # load the sql file and extract cases
-    cr = om.CaseReader(Path(sql_fpath))
-    cases = list(cr.get_cases())
-
-    # initialize results dataframe
+    # initialize results dataframe and counter
     results = pd.DataFrame()
+    ii = 0
 
-    # loop through cases
-    for ii, case in enumerate(cases):
-        if ii == 0:
-            case_results, data_units = summarize_case(case, return_units=True)
-        else:
-            case_results = summarize_case(case, return_units=False)
-        results = pd.concat([results, pd.DataFrame(case_results, index=[ii])], axis=0)
+    # loop through the sql files (only multiple if ran in parallel)
+    for sql_file in sql_files:
+        if "_meta" in sql_file.suffix:
+            # don't read meta data file (only created in ran in parallel)
+            continue
+
+        # load the sql file and extract cases
+        cr = om.CaseReader(Path(sql_file))
+        cases = list(cr.get_cases())
+
+        # loop through cases
+        for case in cases:
+            if ii == 0:
+                case_results, data_units = summarize_case(case, return_units=True)
+            else:
+                case_results = summarize_case(case, return_units=False)
+            results = pd.concat([results, pd.DataFrame(case_results, index=[ii])], axis=0)
+
+            ii += 1
 
     # rename columns to include units
     column_rename_mapper = {
@@ -115,7 +135,6 @@ def convert_sql_to_csv_summary(sql_fpath: Path | str, save_to_file: bool = True)
 
     # save file to csv
     if save_to_file:
-        output_folder = Path(sql_fpath).parent
-        output_fpath = output_folder / f"{sql_fpath.stem}.csv"
+        output_fpath = sql_fpath.parent / f"{sql_fpath.stem}.csv"
         results.to_csv(output_fpath)
     return results
