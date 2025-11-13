5c321ea3fa3524403733a416ec67de7477225446
kbrunik <kbrunik@gmail.com>
2025-11-13 08:01:29 -0800

    remove old test value

--- h2integrate/converters/hydrogen/geologic/test/test_geologic_h2_subsurface.py
+++ h2integrate/converters/hydrogen/geologic/test/test_geologic_h2_subsurface.py
@@ -14,13 +14,12 @@ def test_natural_geoh2(subtests):
             "hydrogen_out"
         )
         assert np.mean(h2_prod) == approx(117.72509205764842, 1e-6)
-    with subtests.test("LCOH"):
-        lcoh = h2i_nat.plant.geoh2.geoh2_finance.get_val("LCOH")
-        assert lcoh == approx(1.2440904, 1e-6)
 
     with subtests.test("integrate LCOH"):
         lcoh = h2i_nat.prob.get_val("finance_subgroup_default.LCOH")
-        assert lcoh == approx(1.72828635, 1e-6)
+        assert lcoh == approx(
+            1.72828635, 1e-6
+        )  # previous val from custom finance model was 1.2440904
 
 
 def test_stimulated_geoh2(subtests):
@@ -34,10 +33,9 @@ def test_stimulated_geoh2(subtests):
 
     with subtests.test("H2 Production"):
         assert np.mean(prod) == approx(155.03934945719536, 1e-6)
-    with subtests.test("LCOH"):
-        lcoh = h2i_stim.plant.geoh2.geoh2_finance.get_val("LCOH")
-        assert lcoh == approx(1.74803827, 1e-6)
 
     with subtests.test("integrate LCOH"):
         lcoh = h2i_stim.prob.get_val("finance_subgroup_default.LCOH")
-        assert lcoh == approx(2.35625412, 1e-6)
+        assert lcoh == approx(
+            2.35625412, 1e-6
+        )  # previous val from custom finance model was 1.74903827
