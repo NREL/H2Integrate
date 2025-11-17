f54f28346ccc8453525d9adcd00ce39f102a4b61
jmartin4 <jonathan.martin@nrel.gov>
2025-11-17 15:03:57 -0700

    Fix JSONDecodeError for real this time

--- h2integrate/converters/hydrogen/test/test_size_modes.py
+++ h2integrate/converters/hydrogen/test/test_size_modes.py
@@ -7,7 +7,7 @@ from h2integrate import EXAMPLE_DIR
 from h2integrate.core.h2integrate_model import H2IntegrateModel
 
 
-def test_size_mode_outputs(subtests):
+def test_resize_by_max_feedstock(subtests):
     # Change the current working directory to the example's directory
     os.chdir(EXAMPLE_DIR / "22_sizing_modes")
 
@@ -26,11 +26,19 @@ def test_size_mode_outputs(subtests):
     model.run()
     model.post_process()
 
-    with subtests.test("Test `resize_by_max_feedstock` mode"):
+    with subtests.test("Check electrolyzer size"):
         assert (
             pytest.approx(model.prob.get_val("electrolyzer.electrolyzer_size_mw")[0], rel=1e-3)
             == 1080
         )
+
+
+def test_resize_by_max_commodity(subtests):
+    # Change the current working directory to the example's directory
+    os.chdir(EXAMPLE_DIR / "22_sizing_modes")
+
+    # Create a H2Integrate model
+    model = H2IntegrateModel(Path.cwd() / "22_size_mode_iterative.yaml")
 
     model.technology_config["technologies"]["electrolyzer"]["model_inputs"][
         "performance_parameters"
@@ -44,7 +52,7 @@ def test_size_mode_outputs(subtests):
     model.run()
     model.post_process()
 
-    with subtests.test("Test `resize_by_max_commodity` mode"):
+    with subtests.test("Check electrolyzer size"):
         assert (
             pytest.approx(model.prob.get_val("electrolyzer.electrolyzer_size_mw")[0], rel=1e-3)
             == 560
